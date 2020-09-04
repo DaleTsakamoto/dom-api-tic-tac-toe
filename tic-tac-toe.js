a7a69f0d8778b869b2becf5fb8a1aafac54fd090
dale.sakamoto <daletsakamoto@gmail.com>
2020-09-04 14:48:13 -0700

    Bullshit quotes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,34 +1,56 @@
 window.addEventListener("DOMContentLoaded", event => {
 
-    //Player Clicks
+    //Variables
     let count = 0;
+    const x = "<img src=\"https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg\">";
+    const o = "<img src= \"https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg\">";;
+    const head = document.getElementById('game-status');
+    const s0 = document.getElementById('square-0');
+    const s1 = document.getElementById('square-1');
+    const s2 = document.getElementById('square-2');
+    
+    //Player clicks
     for (let i = 0; i < 9; i++){
     const playerClick = document.getElementById(`square-${i}`)
-    playerClick.addEventListener("click", event => {
+    playerClick.addEventListener("click", clickSome)
+
 
     //Player Turns - insert X and O
-    // let clicked = document.getElementsByClassName("clicked")
+    function clickSome (event){
+    
     if (count % 2 === 0){
-        playerClick.innerHTML = "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'>"
-        // playerClick.setAttribute("disabled", "disabled")
-        console.log(playerClick.innerHTML.length)
-       console.log( playerClick.removeEventListener("click", ()=> {
-            if (playerClick.innerHTML.length !== 0){}
-
-       }))
+        playerClick.innerHTML = x;
+        playerClick.removeEventListener("click", clickSome)
+        // console.log(s0.innerHTML === x);
+        // console.log(x);
+        // playerClick.id = "x";
         count++;
     } else {
-        playerClick.innerHTML = "<img src= 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'>"
-        // playerClick.setAttribute("disabled", "disabled")
-        playerClick.removeEventListener("click", () => console.log('it worked 2'))
+        playerClick.innerHTML = o;
+        playerClick.removeEventListener("click", clickSome)
+        // playerClick.id = "o";
         count++;
+        }
     }
 
-        console.log (playerClick.innerHTML);
-        if (event.detail < 2) {
+    // if ((s0.innerHTML === x)){
+    //     head.innerHTML = "YOU WIN!"
+    // }
+
+}
+
+for (let i = 0; i < 3; i++){
+    let box = document.getElementById(`square-${i}`)
+    box.addEventListener("click", event => {
+        if ((s0.innerHTML === x) && (s1.innerHTML === x) && (s2.innerHTML === x)){
+            head.innerHTML = "YOU WIN!"
         }
+        console.log (box);
     })
-    }
+
+//     })
+}
+
 
 
 
