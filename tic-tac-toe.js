bc4477009557fcc13265ca7ec1b7dde648d1e728
Jerzy <jerzyhorst@gmail.com>
2020-09-04 11:18:19 -0700

    didn't change much

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,18 +5,25 @@ window.addEventListener("DOMContentLoaded", event => {
     for (let i = 0; i < 9; i++){
     const playerClick = document.getElementById(`square-${i}`)
     playerClick.addEventListener("click", event => {
-        console.log(event.detail);
 
     //Player Turns - insert X and O
+    // let clicked = document.getElementsByClassName("clicked")
     if (count % 2 === 0){
         playerClick.innerHTML = "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'>"
-        playerClick.setAttribute("id", "clicked")
+        // playerClick.setAttribute("disabled", "disabled")
+        console.log(playerClick.innerHTML.length)
+       console.log( playerClick.removeEventListener("click", ()=> {
+            if (playerClick.innerHTML.length !== 0){}
+
+       }))
         count++;
     } else {
-        playerClick.innerHTML = "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'>"
-        playerClick.setAttribute("id", "clicked")
+        playerClick.innerHTML = "<img src= 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'>"
+        // playerClick.setAttribute("disabled", "disabled")
+        playerClick.removeEventListener("click", () => console.log('it worked 2'))
         count++;
     }
+
         console.log (playerClick.innerHTML);
         if (event.detail < 2) {
         }
